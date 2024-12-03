d7b4481ff5c46da4a5f78f1f7733b5aa7d7704d9
Orinda Harrison (Mitch) <mitchke@duck.com>
2024-12-03 10:17:00 +0300

    Initial commit

---
+++ src/main/java/com/github/ajharry69/kcbtechnicalinterview/KcbTechnicalInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.github.ajharry69.kcbtechnicalinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcbTechnicalInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcbTechnicalInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ajharry69/kcbtechnicalinterview/KcbTechnicalInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ajharry69.kcbtechnicalinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcbTechnicalInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
